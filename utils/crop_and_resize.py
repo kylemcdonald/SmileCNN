5c86dec663aa7c73b494a9c97a4a9118ba500e8c
Kyle McDonald <kyle@kylemcdonald.net>
2016-06-08 23:25:33 -0500

    fixed gray/img naming bug

--- utils/crop_and_resize.py
+++ utils/crop_and_resize.py
@@ -2,11 +2,11 @@ import numpy as np
 from skimage.measure import block_reduce
 
 def crop_and_resize(img, target_size=32, zoom=1):
-    small_side = int(np.min(gray.shape) * zoom)
+    small_side = int(np.min(img.shape) * zoom)
     reduce_factor = small_side / target_size
     crop_size = target_size * reduce_factor
-    mid = np.array(gray.shape) / 2
+    mid = np.array(img.shape) / 2
     half_crop = crop_size / 2
-    center = gray[mid[0]-half_crop:mid[0]+half_crop,
-                  mid[1]-half_crop:mid[1]+half_crop]
+    center = img[mid[0]-half_crop:mid[0]+half_crop,
+    	mid[1]-half_crop:mid[1]+half_crop]
     return block_reduce(center, (reduce_factor, reduce_factor), np.mean)
